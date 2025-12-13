2cf1d9a5d8b4997b18c1122ccb44d4c30b2b1aa3
ahammod229 <ahammod229@gmail.com>
2025-12-13 19:53:18 +0600

    Refactor path import and configure Rollup build
    Updated path import and added build configuration for Rollup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import { resolve } from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -16,8 +16,17 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': resolve(__dirname, '.'),
         }
-      }
+      },
+      // ADD THIS SECTION
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            admin: resolve(__dirname, 'admin.html'),
+          },
+        },
+      },
     };
 });
